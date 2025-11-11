9bc5be47a654bac8316a8a4defbd9ecf92f28b12
MV2 <maheshchary660@gmail.com>
2025-11-11 20:48:07 +0530

    Update hello3.py

--- hello3.py
+++ hello3.py
@@ -50,3 +50,116 @@ f=1.8*c+32
 print(f)
 print("welcome to python programming classes")
 print("23")
+
+
+# 100MB
+# 100*10^6 Bytes
+# 1MB -> 1000KB
+# 1Kb -> 1000B
+# 1B -> 8 bits
+# 1 bit -> 0 or 1
+
+# Comments
+# comments are ignored by python interpretor
+# Code readability
+
+# 1. single line - #
+# 2. Multi line - """ or '''
+
+"""
+this is  
+a multi line
+comment
+"""
+
+# 2b=10
+# and=20
+# hello_3=10
+# 3_h=20
+# matchscore=10
+# match_score=10
+# h-2=10
+
+# Data types
+# basic or primitive
+# int,float,string,bool
+
+# defining a variable
+a=10
+
+#accessing a variable
+b=a+2
+print(a)
+
+#modifying a variable
+a=20
+print(a)
+
+b=1.2
+
+s="""hello
+world
+"""
+print(s)
+
+# bool - True or False
+b=True
+b2=False
+
+# type()
+print(type(b))
+print(type(b2))
+print(type(a))
+print(type(s))
+print(type(1.7))
+print(type(1))
+
+# Type conversion/Type casting
+# converting one datatype to another
+s1="21"
+c=int(s1)
+print(c)
+print(type(c))
+print(type(s1))
+s2="10"
+c=int(s2) #"10" -> c=10
+print(c+2)
+# print(s2+2)
+
+s3="10.5"
+c=float(s3)
+print(c)
+
+s4="10"
+c=float(s4)
+print(c)
+
+# what is the data type of return value of float() -> float
+# what is the data type of return value of input() -> string
+# what is the data type of return value of int() -> string
+
+# a=input("Enter something:")
+# print(a)
+
+k=10+20/2
+
+# str()
+a=str(10)
+b=str(4.5)
+c=str('hi')
+d=str(True)
+
+# string operations
+# +(concatenation),*(repetition)
+# + -> only 2 strings can be concatenated
+k=""
+s1="hello"
+s2="world"
+s3=s1+" "+s2
+print(s3)
+
+s4=s1*5
+print(s4)
+
+print(s1+"5")
+# print(s1+5)

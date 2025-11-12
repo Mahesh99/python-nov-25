dd1d3f2458622ab794eb4ab144671d3a6d413158
MV2 <maheshchary660@gmail.com>
2025-11-12 21:54:56 +0530

    Add files via upload

---
+++ loaneligibility.py
@@ -0,0 +1,14 @@
+# age 18 or above
+# salary 20k or above
+age=int(input("Enter your age:"))
+salary=int(input("Enter your salary:"))
+
+# has_property=True
+print(age>=18 and salary>=20000)
+
+# age=17, salary=15000
+# 18>=18 and 15000>=20000
+# True and False
+# False
+
+

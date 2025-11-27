c03f902e1834a52d451daf3daac1671f553a3ad0
MV2 <maheshchary660@gmail.com>
2025-11-27 20:47:29 +0530

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,173 @@
+# conditional statements in Python 
+# if, if-else, if-elif-else
+# indentation is important in Python
+# even odd
+# greater number among two numbers
+# greatest number among three numbers
+
+# if syntax
+"""
+if condition:
+    # code to be executed if condition is True
+"""
+
+# battery=int(input("Enter battery level: ")) # 25 12
+# if battery < 20:
+#     print("Low battery!")
+#     print("Please charge your device.")
+
+# print("some print statement after if block")
+
+
+
+
+
+# if-else syntax
+"""
+if condition:
+    # code to be executed if condition is True
+else:
+    # code to be executed if condition is False
+"""
+
+# number=int(input("Enter a number: ")) # 7 8
+# if number % 2 == 0:
+#     print(f"{number} is even")
+# else:
+#     print(f"{number} is odd")
+
+# print("some print statement after if-else block")
+
+
+
+# loan eligibility
+# age=int(input("Enter your age: "))  # 25 17
+# salary=int(input("Enter your salary: "))  # 50000 15000
+
+# if age >= 18 and salary >= 20000:
+#     print("You are eligible for the loan.")
+# else:
+#     print("You are not eligible for the loan.")
+
+
+
+# if-elif-else syntax
+""" 
+if condition1:
+    # code to be executed if condition1 is True
+elif condition2:
+    # code to be executed if condition2 is True
+else:
+    # code to be executed if both condition1 and condition2 are False
+
+
+else is optional    
+"""
+
+
+# grading system
+# O,A,B,C,D,No grade,Fail
+# marks=int(input("Enter your marks: "))  # 95 85 75 65 55 45 30
+# if marks >= 90:
+#     print("Grade: O")
+# elif marks >= 80:
+#     print("Grade: A")
+# elif marks >= 70:
+#     print("Grade: B")
+# elif marks >= 60:
+#     print("Grade: C")
+# elif marks >= 35:
+#     print("No grade")
+# else:
+#     print("Fail")
+
+# print("some print statement after if-elif-else block")
+
+
+
+
+# nested if
+# number=int(input("Enter a number: "))  # 15 -5 0
+# if number % 3 ==0:
+#     if number % 5==0:
+#         print(f"{number} is divisible by both 3 and 5")
+#     else:
+#         print(f"{number} is divisible by 3 but not by 5")
+#     print(f"{number} is divisible by 3 but not by 5")
+# else:
+#     print(f"{number} is not divisible by 3")
+
+"""
+# what all are considered False in Python?
+constants defined to be false: None and False
+zero of any numeric type: 0, 0.0, 0j, Decimal(0), Fraction(0, 1)
+empty sequences and collections: '', (), [], {}, set(), range(0)
+"""
+
+# Loops in Python
+# for loop, while loop
+
+# when do we use loops?
+# to execute a block of code multiple times
+
+# for loop syntax
+"""
+for variable in iterable:
+    # code to be executed for each item in the iterable
+
+iterable: list, tuple, string, range, etc.
+iterable is an object which can be iterated over (one item at a time)
+"""
+
+# for i in range(100):
+#     print("hello")
+
+s="python programming"
+for c in s:
+    print(f"{c}-hi")
+
+
+l=[10,20,30,40,50]
+for num in l:
+    print(num*2)
+
+s={'one', 'two', 'three'}
+for item in s:
+    print(item)
+
+rollnos=["13311A1247","13311A1248","13311A1249","13311A1250"]
+for no in rollnos:
+    print(no[-2:])
+
+fruits=["apple","banana","cherry"]
+for fruit in fruits:
+    print(fruit.upper())
+
+for fruit in fruits:
+    if fruits.index(fruit)==0:
+        print(fruit.upper())
+    else:
+        print(fruit)
+
+"""
+fruits=["apple","banana","cherry"]
+iteration 1:
+fruit="apple"
+if fruits.index(fruit)==0: => 0==0
+    print(fruit.upper()) <------
+
+iteration 2:
+fruit="banana"
+if fruits.index(fruit)==0: => 1==0
+    print(fruit.upper())
+else:
+    print(fruit)<------
+
+iteration 3:
+fruit="cherry"
+if fruits.index(fruit)==0: => 2==0 
+    print(fruit.upper())
+else:
+    print(fruit)<------
+
+"""

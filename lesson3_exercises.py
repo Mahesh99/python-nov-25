2c6456dd08b59d634c0a1dccbcb0c8ce80393593
MV2 <maheshchary660@gmail.com>
2025-12-08 15:40:31 +0530

    Create lesson3_exercises.py

---
+++ lesson3_exercises.py
@@ -0,0 +1,110 @@
+Exercise 1: Basic Function
+Write a function to return square of a number.
+
+  
+Exercise 2: Default Argument
+Write greet(name) function which prints(sample output: Welcome Ravi), with default value for name as "Guest".
+
+
+Exercise 3: args
+Write a function multiply(*nums) that returns product of all numbers.
+
+  
+Exercise 4: kwargs
+Create a function
+student_info(**data)
+|
+Print details like:
+name: Ram
+age: 20
+course: Python
+
+
+Exercise 5: Mix all argument types
+Write a function with:
+2 positional
+unlimited *args
+1 default
+unlimited **kwargs
+
+Example usage:
+func(10, 20, 1,2,3, c=50, name="Ravi")
+
+
+Exercise 6: Scope
+Write a program showing LEGB example.
+
+
+Exercise 7: Lambda
+Sort this list using lambda by last digit:
+nums = [23, 45, 12, 78, 34]
+
+
+Exercise 8: Recursion
+Write a recursive function to sum digits of a number.
+
+
+
+Exercise 9: Complete the function 
+Write a function that takes a number n and returns the sum of all its digits.
+
+def sum_digits(n):
+    # complete the function
+
+Hint:
+If n = 1234, output should be 10.
+
+
+
+
+
+Exercise 10: Complete the function
+Return the biggest (maximum) value among three given numbers.
+
+def biggest(a, b, c):
+    # return the biggest number
+
+Hint:
+Use simple if/elif/else OR max().
+
+
+
+
+
+
+Exercise 11: Complete the code
+Function receives name, optional age (default=18), and extra details using **info.
+
+def details(name, age=18, **info):
+    # print name, age and other info
+
+Hint:
+Loop through info dictionary.
+
+
+
+
+
+Exercise 12: Complete the lambda
+Create a lambda that returns the cube of a number.
+
+cube = 
+
+Hint:
+cube(3) → 27.
+
+
+
+
+
+Exercise 13: Complete the nested function
+Inner function should print message from outer function.
+  
+def outer():
+    message = "Hello"
+    def inner():
+        # print message
+    inner()
+
+Hint:
+Variable is available from enclosing scope.
